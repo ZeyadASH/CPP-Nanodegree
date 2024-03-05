128b21ce9d1e1f6257181abeb45af401afeb8868
ZeyadASH <123611671+ZeyadASH@users.noreply.github.com>
2024-03-05 21:41:04 +0200

    Update main.cpp

--- 2_Foundations/02_Introduction_to_the_C++_Language/12_Using_Auto/main.cpp
+++ 2_Foundations/02_Introduction_to_the_C++_Language/12_Using_Auto/main.cpp
@@ -10,11 +10,11 @@ using std::vector;
 using std::cout;
 
 int main() {
-    auto i = 5;
+    //auto i = 5;
     auto v_6 = {1, 2, 3,7,8,9};
     cout << "Variables declared and initialized without explicitly stating type!" << "\n";
 
-    for (auto i :v_6){
+    for (auto i :v_6){        //A range based loop
         cout << i << " ";
     }
     cout << "\n";
