f99ecc6ef1f5d716a611b5617fadb768aec7cc1e
ZeyadASH <123611671+ZeyadASH@users.noreply.github.com>
2024-03-05 23:42:05 +0200

    Update main.cpp

--- 2_Foundations/02_Introduction_to_the_C++_Language/15_Working_with_Vectors/main.cpp
+++ 2_Foundations/02_Introduction_to_the_C++_Language/15_Working_with_Vectors/main.cpp
@@ -1,32 +1,39 @@
-
 #include <iostream>
 #include <vector>
+
 using std::vector;
 using std::cout;
 
 int main() {
-
+    
     //////////////////////
     //1D Vector Access
     /////////////////////
+    
     vector<int> a = {0, 1, 2, 3, 4};
-    cout << a[0];
-    cout << a[1];
-    cout << a[2];
-    cout << "\n";
+    cout << "The First Element of Vector a is " << a[0] <<"\n";
+    cout << "The Second Element of Vector a is " << a[1] <<"\n";
+    cout << "The Third Element of Vector a is " << a[2] <<"\n";
+    
     //Getting a Vector's Length
-    cout << a.size()<<"\n";
+    cout << "The Length of Vector a is 1x"<< a.size()<<"\n";
 
     ////////////////////////
     //2D Vector Access
     ///////////////////////
-    vector<vector<int>> b = {{1, 1, 2, 3},
-                             {2, 1, 2, 3},
-                             {3, 1, 2, 3}};
-
-    cout << b[2][1];
-    cout << "\n";
+    
+    vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 0, 0, 0, 1, 0}};
+                                 
+    cout << "The Element in third row second column is of Vector board is " 
+         << board[2][1] << "\n";
+         
+    
     //Getting a Vector's Length
-    cout << b.size()<<"x"<<b[0].size()<<"\n";
-
+    cout << "The Length of Vector board is " 
+         << board.size()<<"x"<<board[0].size()<<"\n";
+    
 }
